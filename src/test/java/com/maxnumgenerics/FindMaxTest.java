4334910e91d82f2c04661b7db82617f6f26994a1
shailendra28 <yshailendrakumar1768@gmail.com>
2021-06-21 03:32:27 +0530

    Refactor Code and Test Case

--- src/test/java/com/maxnumgenerics/FindMaxTest.java
+++ src/test/java/com/maxnumgenerics/FindMaxTest.java
@@ -6,77 +6,55 @@ import org.junit.jupiter.api.Test;
  *  here in this test class I have created an object of main class and call all the methods of test cases.
  */
 public class FindMaxTest {
-    // I have created an Object of FindMaximum class
-    TestMaximum testMaxObj = new TestMaximum();
+    FindMaximum findMaxObj = new FindMaximum();
     /**
-     * Here I have created 12 test cases which will check the max and according to the test cases it'll get passed.
+     * Here we have the same code only I have used E as a type.
+     * that E will automatically fetch any datatypes.
      */
     @Test
     void ifIntegerNumberOneIsMaxReturnTrue() {
-        TestMaximum<Integer> testMaximumInteger = new TestMaximum<>(100, 50, 25);
-        Assertions.assertEquals(100, testMaximumInteger.testMaximum());
+
+        Assertions.assertEquals(100, findMaxObj.findMax(100, 50, 25));
     }
 
     @Test
     void ifIntegerNumberTwoIsMaxReturnTrue() {
-        TestMaximum<Integer> testMaximumInteger = new TestMaximum<>(50, 100, 25);
-        Assertions.assertEquals(100, testMaximumInteger.testMaximum());
+
+        Assertions.assertEquals(100, findMaxObj.findMax(50, 100, 25));
     }
 
     @Test
     void ifIntegerNumberThreeIsMaxReturnTrue() {
-        TestMaximum<Integer> testMaximumInteger = new TestMaximum<>(25, 50, 100);
-        Assertions.assertEquals(100, testMaximumInteger.testMaximum());
+        Assertions.assertEquals(100, findMaxObj.findMax(25, 50, 100));
     }
 
     @Test
     void ifFloatNumberOneIsMaxReturnTrue() {
-        TestMaximum<Float> testMaximumInteger = new TestMaximum<>(10.0F, 5.0F, 2.5F);
-        Assertions.assertEquals(10.0F, testMaximumInteger.testMaximum());
+        Assertions.assertEquals(10.0F, findMaxObj.findMax(10.0F, 5.0F, 2.5F));
     }
 
     @Test
     void ifFloatNumberTwoIsMaxReturnTrue() {
-        TestMaximum<Float> testMaximumInteger = new TestMaximum<>(5.0F, 10.0F, 2.5F);
-        Assertions.assertEquals(10.0F, testMaximumInteger.testMaximum());
+        Assertions.assertEquals(10.0F, findMaxObj.findMax(5.0F, 10.0F, 2.5F));
     }
 
     @Test
     void ifFloatNumberThreeIsMaxReturnTrue() {
-        TestMaximum<Float> testMaximumInteger = new TestMaximum<>(2.5F, 5.0F, 10.0F);
-        Assertions.assertEquals(10.0F, testMaximumInteger.testMaximum());
+        Assertions.assertEquals(10.0F, findMaxObj.findMax(2.5F, 5.0F, 10.0F));
     }
 
     @Test
     void ifStringOneIsMaxReturnTrue() {
-        TestMaximum<String> testMaximumInteger = new TestMaximum<>("banana", "peach", "apple");
-        Assertions.assertEquals("peach", testMaximumInteger.testMaximum());
+        Assertions.assertEquals("peach", findMaxObj.findMax("banana", "apple", "peach"));
     }
 
     @Test
     void ifStringTwoIsMaxReturnTrue() {
-        TestMaximum<String> testMaximumInteger = new TestMaximum<>("apple", "banana", "peach");
-        Assertions.assertEquals("peach", testMaximumInteger.testMaximum());
+        Assertions.assertEquals("peach", findMaxObj.findMax("apple", "banana", "peach"));
     }
 
     @Test
     void ifStringThreeIsMaxReturnTrue() {
-        TestMaximum<String> testMaximumInteger = new TestMaximum<>("peach", "apple", "banana");
-        Assertions.assertEquals("peach", testMaximumInteger.testMaximum());
-    }
-
-    @Test
-    void giveMaxInteger() {
-        Assertions.assertEquals(60, testMaxObj.testMaximumMore(1, 10, 13, 17, 25, 43, 21, 56, 60));
-    }
-
-    @Test
-    void giveMaxFloat() {
-        Assertions.assertEquals(60.0F, testMaxObj.testMaximumMore(1.0F, 10.0F, 13.0F, 1.70F, 2.05F, 4.3F, 21.0F, 56.0F, 60.0F));
-    }
-
-    @Test
-    void giveMaxString() {
-        Assertions.assertEquals("peach", testMaxObj.testMaximumMore("apple", "peach", "banana", "mango", "kiwi"));
+        Assertions.assertEquals("peach", findMaxObj.findMax("peach", "apple", "banana"));
     }
 }
